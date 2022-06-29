c194dd5adfb5093ef4fe2d24b2df200ea2ae65f7
mrhouzlane <mrhouzlane@github.com>
2022-06-29 00:50:26 -0400

    contracts tested

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,34 @@
+require("dotenv").config();
+
+require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const networks = require("./networks.json");
+const accounts = process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [];
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      finder: networks.rinkeby.contracts.finder,
+      chainId: networks.rinkeby.chainId,
+      url: networks.rinkeby.rpc,
+      accounts,
+    },
+    optimism: {
+      finder: networks.optimism.contracts.finder,
+      chainId: networks.optimism.chainId,
+      url: networks.optimism.rpc,
+      accounts,
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };

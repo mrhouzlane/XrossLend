5d2a5bc3e31c7c90a6b1827ef78c4bd3eb3087ea
Taiju Sanagi <taijusanagi@gmail.com>
2022-06-30 07:59:19 +0900

    added ipfs

--- web/src/index.js
+++ web/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { RecoilRoot } from "recoil";
 
@@ -14,9 +14,9 @@ root.render(
   <React.StrictMode>
     <ChakraProvider>
       <RecoilRoot>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </RecoilRoot>
     </ChakraProvider>
   </React.StrictMode>

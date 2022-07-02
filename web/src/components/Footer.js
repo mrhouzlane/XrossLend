04821f89bc7ffd9f9f60f47b0d38d10fbdaa155a
mrhouzlane <mrhouzlane@github.com>
2022-07-02 00:46:12 -0400

    ok

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <Flex minH={"64px"} alignItems={"center"} justifyContent={"center"} px="4" py="8" gap={"16px"}>
       <Text fontSize={"xs"} fontWeight={"medium"} color="red.400">
-        XrossLend@ETHNewYork
+        XrossLend
       </Text>
     </Flex>
   );
